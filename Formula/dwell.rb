dd211db0483a9808edea04e4511dbdefa795fafa
wess <wcope@me.com>
2021-08-17 15:29:37 -0400

    Forumla for dwell added

---
+++ Formula/dwell.rb
@@ -0,0 +1,17 @@
+class Dwell < Formula
+  desc "Virtualenv for Rust in sweet, sweet, bash."
+  homepage ""
+  url "https://github.com/wess/dwell/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "787073752ef15208dd404894c4b51c4d5655f38611693e3647e35c61cab3e5c5"
+  license "MIT"
+
+  def install
+    libexec.install Dir["libexec/*"]
+
+    bin.write_exec_script (libexec/"dwell")
+  end
+
+  test do
+    system "true"
+  end
+end
