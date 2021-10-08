d81b3c3aaa1011ab2d0c5076af3b341452f9750e
wess <wcope@me.com>
2021-10-08 13:02:27 -0400

    Hatch bump to 0.0.2

--- Formula/hatch.rb
+++ Formula/hatch.rb
@@ -1,8 +1,8 @@
 class Hatch < Formula
   desc "Little shell script magic to spruce up my new project directory."
   homepage ""
-  url "https://github.com/wess/hatch/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "2ac1fa7baefa1c4314e477657631fcd5ea55500f8332fed0feaa43ff6785dc7a"
+  url "https://github.com/wess/hatch/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "20ad35f522225a7abdcc2dcf8a5fc33c2183c9f1c37de3ace9e83be302f02379"
   license "MIT"
 
   def install
