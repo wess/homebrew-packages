8e7c2492fc1a359dffe580ccb9cdb2072ec2dce9
wess <wcope@me.com>
2021-12-17 14:06:16 -0500

    Updates shasum

--- Formula/motive.rb
+++ Formula/motive.rb
@@ -2,13 +2,13 @@ class Motive < Formula
   desc "Command-line task runner and project manager"
   homepage "https://github.com/wess/motive"
   url "https://github.com/wess/motive/releases/download/0.0.2/motive-0.0.2-x86_64-apple-darwin.tar.gz"
-  sha256 "3400b72ca5b0198c9644ec6b9cb1f0c751e7d0a4dedaf0dc8338a7dacb3ac51c"
+  sha256 "8c5eb891e85db1bd515a5b822b5777927c99bf019ed26d5c12b98c7f054b8943"
 
   def install
     bin.install "motive"
   end
 
-  def test
+  test do
     system "motive", "--version"
   end
 end
