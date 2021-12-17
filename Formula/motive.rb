4a0a3ae75310fd0d4d54f764cbbaccbf3a3c1c06
wess <wcope@me.com>
2021-12-17 14:01:45 -0500

    Motive 0.0.2 release

--- Formula/motive.rb
+++ Formula/motive.rb
@@ -1,7 +1,7 @@
 class Motive < Formula
   desc "Command line task runner and project manager"
   homepage "https://github.com/wess/motive"
-  url "https://github.com/wess/motive/releases/download/0.0.1/motive-0.0.1-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/wess/motive/releases/download/0.0.2/motive-0.0.2-x86_64-apple-darwin.tar.gz"
   sha256 "3400b72ca5b0198c9644ec6b9cb1f0c751e7d0a4dedaf0dc8338a7dacb3ac51c"
 
   def install
