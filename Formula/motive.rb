7763edc6d50b1e29a579c4f45836b0b013adb19f
Wess Cope <wcope@me.com>
2022-02-11 15:38:54 -0500

    Update motive.rb

--- Formula/motive.rb
+++ Formula/motive.rb
@@ -1,8 +1,8 @@
 class Motive < Formula
   desc "Command-line task runner and project manager"
   homepage "https://github.com/wess/motive"
-  url "https://github.com/wess/motive/releases/download/0.0.2/motive-0.0.2-x86_64-apple-darwin.tar.gz"
-  sha256 "8c5eb891e85db1bd515a5b822b5777927c99bf019ed26d5c12b98c7f054b8943"
+  url "https://github.com/wess/motive/releases/download/0.0.1/motive-0.0.1-x86_64-apple-darwin.tar.gz"
+  sha256 "6446697762d8e42c4fad596205cbccd2cb86825f5cf327e48a6ca4c6b42393c9"
 
   def install
     bin.install "motive"
