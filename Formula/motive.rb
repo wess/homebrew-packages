6c2ef1e5b0bf0225e5c15ec6a0fe2f022b448033
Wess Cope <wcope@me.com>
2022-03-03 08:43:44 -0500

    Release 0.0.3
    Still in early stages but now with a formal parsing engine for Lua subset instead of string replace hacks 😄

--- Formula/motive.rb
+++ Formula/motive.rb
@@ -1,8 +1,8 @@
 class Motive < Formula
   desc "Command-line task runner and project manager"
   homepage "https://github.com/wess/motive"
-  url "https://github.com/wess/motive/releases/download/0.0.2/motive-0.0.2-x86_64-apple-darwin.tar.gz"
-  sha256 "7aa3f8ed6c061f359bf58f677986db78800c69915b53a921c25400d148bd782e"
+  url "https://github.com/wess/motive/releases/download/0.0.3/motive-0.0.3-x86_64-apple-darwin.tar.gz"
+  sha256 "db53e29d1d36d03d3cc735fae04d8bb59ca5709e73772ba2209c60b1f7d4d2ce"
 
   def install
     bin.install "motive"
