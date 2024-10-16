039aa18a5f0eb00f288b4a29364a08bdeefcc3a2
Wess Cope <me@wess.io>
2024-10-16 13:33:27 -0400

    Updates opm recipe

--- Formula/opm.rb
+++ Formula/opm.rb
@@ -2,15 +2,21 @@ class Opm < Formula
   desc "A CLI app to manage OpenAPI projects"
   homepage "https://github.com/wess/opm"
   url "https://github.com/wess/opm/archive/refs/heads/main.zip"
-  version "0.0.1"
+  version "0.0.2"
   sha256 :no_check
 
   depends_on "bun" => :build
 
   def install
-    # Download the repo and build it
-    system "bun", "install"
-    system "bun", "run", "release"
+    # Locate the path to bun
+    bun_path = `which bun`.chomp
+    if bun_path.empty?
+      odie "Bun is required but not installed or not in the PATH."
+    end
+
+    # Install dependencies and build the project
+    system bun_path, "install"
+    system bun_path, "run", "release"
 
     # Install the executable
     bin.install "dist/opm"
