c06958c658af52372e56f86887da36afc8150ba0
Wess Cope <me@wess.io>
2025-06-27 15:26:58 -0400

    Zenoscript release 0.1.0

--- Formula/zenoscript.rb
+++ Formula/zenoscript.rb
@@ -1,14 +1,19 @@
 class Zenoscript < Formula
   desc "Bringing Functional-ish to Typescript"
   homepage "https://github.com/wess/zenoscript"
-  version "0.0.1"
+  version "0.1.0"
 
   if OS.mac?
-    url "https://github.com/wess/zenoscript/releases/download/v#{version}/zenoscript-macos.tar.gz"
-    sha256 "995337fc6dd1039b83f1daecb717c9e79996d3019ccc5040496cc85517f966eb"
+    if Hardware::CPU.intel?
+      url "https://github.com/wess/zenoscript/releases/download/v#{version}/zenoscript-darwin-x64.tar.gz"
+      sha256 "42a71067638506f01b899e8abf6f0e408d389d6eaa86bf23a6e42e462ad538ce"
+    else # ARM
+      url "https://github.com/wess/zenoscript/releases/download/v#{version}/zenoscript-darwin-arm64.tar.gz"
+      sha256 "5480067890f3b323b9ae1c7e59b499a80c67c34674eccbc0717a76622c7beb4d"
+    end
   elsif OS.linux?
-    url "https://github.com/wess/zenoscript/releases/download/v#{version}/zenoscript-linux.tar.gz"
-    sha256 "bf97d84e6621e6c4178d71b424b2f1c7e2c54361735c0e484b330bace423f035"
+    url "https://github.com/wess/zenoscript/releases/download/v#{version}/zenoscript-linux-x64.tar.gz"
+    sha256 "3d1b4b0ea3aab764a5e7f1b9d197a43e378db9520a1af206f4f53d3944945dde"
   end
 
   def install
