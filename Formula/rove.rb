de5d545ace2f3797e73761d9ed6e1b51b0e6a18d
Wess Cope <me@wess.io>
2025-07-28 11:47:29 -0400

    Update adds Seeding and MCP server making your db llm/agent friendly

--- Formula/rove.rb
+++ Formula/rove.rb
@@ -1,19 +1,19 @@
 class Rove < Formula
   desc "PostgreSQL migrations tool built with Bun and TypeScript"
   homepage "https://github.com/wess/rove"
-  version "0.0.4"
+  version "0.0.5"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/wess/rove/releases/download/v#{version}/rove-#{version}-darwin-x64.tar.gz"
-      sha256 "89bf06b2ab478db50fd1603777f31d984b391f64673feaab7c99d0c98ae818ac"
+      sha256 "73703c5570d9b62b783b9d87e8c31aa9bbb569eee832530ce16e2737b57fb997"
     else # ARM
       url "https://github.com/wess/rove/releases/download/v#{version}/rove-#{version}-darwin-arm64.tar.gz"
-      sha256 "adb853da6a0fb57ae7f26bcc169bb32c5011f88298de2a8becec3dacfade7ed6"
+      sha256 "90a47816fe0e00dbfeb488837d430f3b057ee4b10124d49312e1e42e310104e8"
     end
   elsif OS.linux?
     url "https://github.com/wess/rove/releases/download/v#{version}/rove-#{version}-linux-x64.tar.gz"
-    sha256 "3d1b4b0ea3aab764a5e7f1b9d197a43e378db9520a1af206f4f53d3944945dde"
+    sha256 "ff0524fc638c680bdeaedfd06d67bc56107bf09cc2cf10ad59bf1c7d79c4b9c4"
   end
 
   def install
--- Formula/zenoscript.rb
+++ Formula/zenoscript.rb
@@ -1,19 +1,19 @@
 class Zenoscript < Formula
   desc "Bringing Functional-ish to Typescript"
   homepage "https://github.com/wess/zenoscript"
-  version "0.1.4"
+  version "0.1.5"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/wess/zenoscript/releases/download/v#{version}/zenoscript-darwin-x64.tar.gz"
-      sha256 "084a4dc4c09567e39fb9c7cb0d127f4a8a9918dcb935d7d53c18c1a6955d634f"
+      sha256 "95439ac705a2eda76dd26e9cb086b6460b57d1f1e09d1d892fbda47ae122da08"
     else # ARM
       url "https://github.com/wess/zenoscript/releases/download/v#{version}/zenoscript-darwin-arm64.tar.gz"
-      sha256 "aa760519cfbea9af999d71fa45fa03787efbe79181f690375c603d4a6db2f8fb"
+      sha256 "e42f1c46c164e7f801a3bd7a6d1283a57caa4ea4be351786aecd18a7dac2fd07"
     end
   elsif OS.linux?
     url "https://github.com/wess/zenoscript/releases/download/v#{version}/zenoscript-linux-x64.tar.gz"
-    sha256 "7ddec4997f4b00f7a13c716754b4afacd793babcb9ce5e70cf45afcce62413a7"
+    sha256 "fc18940f83f646949c543855f7e589090a9175d63e79cf40630ac99802c3e137"
   end
 
   def install
