eb256ff2f37f6a5ed330bea8ea6645dd441ee184
Wess Cope <me@wess.io>
2026-04-28 17:19:40 -0400

    Bump ambry to v0.2.1 (macOS ARM only)

--- Formula/ambry.rb
+++ Formula/ambry.rb
@@ -1,39 +1,30 @@
 class Ambry < Formula
   desc "Open-source, cross-platform database client"
   homepage "https://github.com/wess/ambry"
-  version "0.2.0"
+  url "https://github.com/wess/ambry/releases/download/v0.2.1/ambry-darwin-arm64.tar.gz"
+  version "0.2.1"
+  sha256 "d634c8bdc788fb9a35a28359acf4e981258e4cf8000ccfa60e43d82b40a21ff3"
   license "MIT"
 
-  if OS.mac?
-    url "https://github.com/wess/ambry/releases/download/v#{version}/ambry-darwin-arm64.tar.gz"
-    sha256 "69c8496f011a7c916d3d6c4cf23627ee1880bb3cc8b7a25588dfc1c12abfd309"
-  elsif OS.linux?
-    url "https://github.com/wess/ambry/releases/download/v#{version}/ambry-linux-x64.tar.gz"
-    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
-  end
+  depends_on arch: :arm64
+  depends_on :macos
 
   def install
-    if OS.mac?
-      prefix.install "Ambry.app"
-      bin.write_exec_script prefix/"Ambry.app/Contents/MacOS/ambry"
-    else
-      bin.install "ambry"
-    end
+    prefix.install "Ambry.app"
+    bin.write_exec_script prefix/"Ambry.app/Contents/MacOS/ambry"
   end
 
   def caveats
-    if OS.mac?
-      <<~EOS
-        Ambry.app has been installed to:
-          #{prefix}/Ambry.app
+    <<~EOS
+      Ambry.app has been installed to:
+        #{prefix}/Ambry.app
 
-        To add it to Applications:
-          ln -sf #{prefix}/Ambry.app /Applications/Ambry.app
-      EOS
-    end
+      To add it to Applications:
+        ln -sf #{prefix}/Ambry.app /Applications/Ambry.app
+    EOS
   end
 
   test do
-    assert_predicate bin/"ambry", :exist?
+    assert_path_exists bin/"ambry"
   end
 end
