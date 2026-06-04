b771833beb1b3f669e1f3bc2ac99afa425859ed2
Wess Cope <apple@wess.io>
2026-06-04 12:07:41 -0400

    Update butter/stim/nenv formulae; remove unmaintained formulae

--- Formula/butter.rb
+++ Formula/butter.rb
@@ -1,19 +1,23 @@
 class Butter < Formula
   desc "Lightweight desktop app framework with native webview and TypeScript"
   homepage "https://github.com/wess/butter"
-  version "1.1.2"
+  version "1.6.2"
 
-  if OS.mac?
-    if Hardware::CPU.intel?
-      url "https://github.com/wess/butter/releases/download/v#{version}/butter-darwin-x64.tar.gz"
-      sha256 "8bd2ae23192ddee017bf2fa22cabaeb745bd11d45a14c1f077f6c4faddde0677"
-    else # ARM
-      url "https://github.com/wess/butter/releases/download/v#{version}/butter-darwin-arm64.tar.gz"
-      sha256 "60e94e05f716dcb5f26477887f800fbf8a17e54378e007e12ef0e17ebcd6db02"
+  on_macos do
+    depends_on arch: :arm64
+    url "https://github.com/wess/butter/releases/download/v#{version}/butter-darwin-arm64.tar.gz"
+    sha256 "ebea401a16b3d3d35c6301d05ffec269b98182674dd0d503770498a7bad6c4c5"
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/wess/butter/releases/download/v#{version}/butter-linux-arm64.tar.gz"
+      sha256 "6e5d57fa2357c78cd3d2a5b5011b077ac2bb40a6eca87edb76e49a95edb7b14d"
     end
-  elsif OS.linux?
-    url "https://github.com/wess/butter/releases/download/v#{version}/butter-linux-x64.tar.gz"
-    sha256 "1bbca80309c808258fa8c490ead51c61c7d2d1591855debfd58a1c6978d59aa0"
+    on_intel do
+      url "https://github.com/wess/butter/releases/download/v#{version}/butter-linux-x64.tar.gz"
+      sha256 "498f293aacf4c30b5faf44be36657d572df04b9c3ae800b59ce6f22bb8360d8e"
+    end
   end
 
   def install
--- Formula/stim.rb
+++ Formula/stim.rb
@@ -1,19 +1,19 @@
 class Stim < Formula
   desc "A little language to make prompts more program-y"
   homepage "https://github.com/wess/stim"
-  version "1.0.1"
+  version "1.2.1"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/wess/stim/releases/download/v#{version}/stim-darwin-x64"
-      sha256 "eb60ef966d040934b1dbf2c2548da90604c9c4f1c33024e62411d654c752042b"
+      sha256 "884ab374b47bf4efd8d2a60ad9a49153de52af962bdc9d7da061a4d9051491de"
     else # ARM
       url "https://github.com/wess/stim/releases/download/v#{version}/stim-darwin-arm64"
-      sha256 "3f22594419de50968d2a908837d85337bd82b4804d81fe056255689f6e70417a"
+      sha256 "858522d829136c5efebd9941a26f16c500b0998748fc105d1219d6356ee2b138"
     end
   elsif OS.linux?
     url "https://github.com/wess/stim/releases/download/v#{version}/stim-linux-x64"
-    sha256 "533f46c965719d7e923e56dc3a69bd4543fb1089c6ef0c7f28e2714fbf2bf8fd"
+    sha256 "b8ec5d987b9442ccea7f814fe9b90416bde3bdd692ff2c4095de7f60dd852aba"
   end
 
   def install
