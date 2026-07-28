1419c8ee6a2cf22040790817c3fcab0f62c82a4a
Wess Cope <wcope@me.com>
2026-07-28 15:54:39 -0400

    Add synapse v0.1.0-beta.1

---
+++ Casks/synapse.rb
@@ -0,0 +1,21 @@
+cask "synapse" do
+  version "0.1.0-beta.1"
+  sha256 "1ec9f0d91cabe66d287f5a0ba284aae32a126bc2b38ad707c4f8a11980e839aa"
+
+  url "https://github.com/wess/synapse/releases/download/v#{version}/synapse.zip"
+  name "Synapse"
+  desc "Local memory and scoped credentials for developer tools"
+  homepage "https://wess.io/synapse/"
+
+  depends_on arch: :arm64
+  depends_on macos: :ventura
+
+  app "synapse.app"
+  binary "#{appdir}/synapse.app/Contents/MacOS/synapse"
+
+  zap trash: [
+    "~/Library/Application Support/synapse",
+    "~/Library/Preferences/app.synapse.desktop.plist",
+    "~/Library/Saved Application State/app.synapse.desktop.savedState",
+  ]
+end
