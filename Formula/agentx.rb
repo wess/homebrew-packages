9207c245755e50304f4a7be17db7520e2c2ff090
Wess Cope <wcope@me.com>
2026-09-02 09:43:44 -0400

    Update agentx to 0.1.2

--- Formula/agentx.rb
+++ Formula/agentx.rb
@@ -7,23 +7,23 @@ class Agentx < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/wess/agentx/releases/download/v0.1.1/agentx-0.1.1-aarch64-apple-darwin.tar.gz"
-      sha256 "4ef7945a00ef4fcb1bb0edcc178bc0904d4a09f11b2e5ee6627bed92f6fef743"
+      url "https://github.com/wess/agentx/releases/download/v0.1.2/agentx-0.1.2-aarch64-apple-darwin.tar.gz"
+      sha256 "da78dfa48254e05fa16e262a60c36cedb55baf7178215beff9f27c662b87565c"
     end
     on_intel do
-      url "https://github.com/wess/agentx/releases/download/v0.1.1/agentx-0.1.1-x86_64-apple-darwin.tar.gz"
-      sha256 "a31166d4eded7f7b24d8c40dc45a5f7177c598db5b0eba282539802824c466b8"
+      url "https://github.com/wess/agentx/releases/download/v0.1.2/agentx-0.1.2-x86_64-apple-darwin.tar.gz"
+      sha256 "3a6afae627227a526e4a48f09069d217e198e5227534cf0039daad630bc87616"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/wess/agentx/releases/download/v0.1.1/agentx-0.1.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "7426bb4396e74088ee827db09313e9da0170fa2adbd28492b3b8b21f7fe91bf6"
+      url "https://github.com/wess/agentx/releases/download/v0.1.2/agentx-0.1.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "f6b91a396f38b4f6a58e4070b1b46a0e99aa8df73b60da6e9d401c46355ec305"
     end
     on_intel do
-      url "https://github.com/wess/agentx/releases/download/v0.1.1/agentx-0.1.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "4f5ef27713699d650e72a10ce363b5947fa295efa3e0907b89e1e7fda9b5fa5b"
+      url "https://github.com/wess/agentx/releases/download/v0.1.2/agentx-0.1.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "be15d9fcd91c52e2d7d6caed0d495c69ebc063dc325504fbaab9a11b249e9d12"
     end
   end
 
