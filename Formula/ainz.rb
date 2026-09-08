7b9dd58247eddd62bde1614fc870e7f51e4ab913
Wess Cope <apple@wess.io>
2026-09-08 12:45:53 -0400

    Update Ainz to 0.9.2
    Ship Lua plugins and the security and stability fixes using the four published archives and verified SHA-256 checksums.

--- Formula/ainz.rb
+++ Formula/ainz.rb
@@ -7,23 +7,23 @@ class Ainz < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/wess/ainz/releases/download/v0.8.1/ainz-0.8.1-aarch64-apple-darwin.tar.gz"
-      sha256 "5ede092fe7756582255262518b2c2d0a3673200ece5ae9ce5d174300a0426aeb"
+      url "https://github.com/wess/ainz/releases/download/v0.9.2/ainz-0.9.2-aarch64-apple-darwin.tar.gz"
+      sha256 "50733dfef2620d53de7c8840eea8729969c881a17a053dc6636f410c492f111b"
     end
     on_intel do
-      url "https://github.com/wess/ainz/releases/download/v0.8.1/ainz-0.8.1-x86_64-apple-darwin.tar.gz"
-      sha256 "b0ec92eab01be48848195af64338da718a2c72644d911aa82319fc435340eec2"
+      url "https://github.com/wess/ainz/releases/download/v0.9.2/ainz-0.9.2-x86_64-apple-darwin.tar.gz"
+      sha256 "6e26c43a9c4fc7a5aed3e660c9af7ab0fc15b4a130cd8835a041ad4a89e120f2"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/wess/ainz/releases/download/v0.8.1/ainz-0.8.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "5b3605abc4aad6b28c0d830787445034daa7fda593e61fd20ac09c975d66b0a5"
+      url "https://github.com/wess/ainz/releases/download/v0.9.2/ainz-0.9.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "0b85293c023eb6592dd54c7e41d8b02e1e3998ab7ec6fad6e199e0e7d97e2292"
     end
     on_intel do
-      url "https://github.com/wess/ainz/releases/download/v0.8.1/ainz-0.8.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "82c4f743dadbd8672f1bd29c534530eae76859f0cdfc3cf0d0a3758bdc1c35b6"
+      url "https://github.com/wess/ainz/releases/download/v0.9.2/ainz-0.9.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "c47ce04b15d953f69e452eb4d89c45e1a1d535b38c1542887859eb170324c869"
     end
   end
 
